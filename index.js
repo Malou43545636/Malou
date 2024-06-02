3a0bbefa84fee1d5a2b325af2cfb8e3dd4a55d57
Malou43545636 <berzanlilleby@gmail.com>
2024-06-02 13:58:56 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+addEventListener("DOMContentLoaded",  () =>  {
+  let button = document.querySelector(".button");
+
+
+  button.addEventListener("click", () => {
+alert("Tyvärr finns det inget mer att se. Kontakta Mr. Beetge för mer utveckling av hemsidan")
+})
+})
